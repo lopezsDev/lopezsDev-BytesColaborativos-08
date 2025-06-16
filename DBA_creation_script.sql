e11c6195dd9d7be961fbfba1ca2de71887215ee3
Max Jeldres <=>
2025-06-15 22:29:54 -0400

    add: script to start creation dba

---
+++ DBA_creation_script.sql
@@ -0,0 +1,11 @@
+-- On linux terminla => sudo -u postgres psql
+
+-- first in youe terminal on your pgAdmin
+
+CREATE DATABASE IF NOT EXISTS "gameHub";
+
+CREATE USER admin_gh WITH PASSWORD 'admin1234';
+
+GRANT ALL PRIVILEGES ON DATABASE gameHub TO admin_gh;
+
+-- Create tables on models
