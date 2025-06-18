a91bb5901123fa1fb4db4313a03f6b49e57e5937
User <lopezs.dev@gmail.com>
2025-06-17 22:59:59 -0600

    feat: create AuthService interface

--- gamehub/src/main/java/com/game/hub/gamehub/services/AuthService.java
+++ gamehub/src/main/java/com/game/hub/gamehub/services/AuthService.java
@@ -1,4 +1,13 @@
 package com.game.hub.gamehub.services;
 
-public class AuthService {
+import com.game.hub.gamehub.dtos.LoginRequest;
+import com.game.hub.gamehub.dtos.LoginResponse;
+import com.game.hub.gamehub.dtos.RegisterRequest;
+import com.game.hub.gamehub.dtos.RegisterResponse;
+
+public interface AuthService {
+
+    RegisterResponse register(RegisterRequest request);
+    
+    LoginResponse login(LoginRequest request);
 }
