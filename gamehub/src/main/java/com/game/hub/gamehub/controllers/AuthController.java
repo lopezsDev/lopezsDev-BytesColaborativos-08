808bb1512a438828b5d71b3c9386e63cc167b020
User <lopezs.dev@gmail.com>
2025-06-17 22:18:31 -0600

    feat: add base structure for AuthController

---
+++ gamehub/src/main/java/com/game/hub/gamehub/controllers/AuthController.java
@@ -0,0 +1,11 @@
+package com.game.hub.gamehub.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+
+}
