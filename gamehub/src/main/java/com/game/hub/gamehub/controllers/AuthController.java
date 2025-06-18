09d550b833bfb2716f4def543dfc1edf8c71d4cc
User <lopezs.dev@gmail.com>
2025-06-17 23:25:14 -0600

    refactor: delete unused import

--- gamehub/src/main/java/com/game/hub/gamehub/controllers/AuthController.java
+++ gamehub/src/main/java/com/game/hub/gamehub/controllers/AuthController.java
@@ -5,7 +5,6 @@ import com.game.hub.gamehub.dtos.LoginResponse;
 import com.game.hub.gamehub.dtos.RegisterRequest;
 import com.game.hub.gamehub.dtos.RegisterResponse;
 import com.game.hub.gamehub.services.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
