8e895aee3e7ec6aade29a0565b316b7dc0a85cb6
Max Jeldres <max.4ndr3@gmail.com>
2025-06-19 00:48:48 -0400

    refactor: JwtAuth... and SecConf... without error to run server

--- gamehub/src/main/java/com/game/hub/gamehub/config/SecurityConfig.java
+++ gamehub/src/main/java/com/game/hub/gamehub/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.game.hub.gamehub.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,11 +22,13 @@ public class SecurityConfig {
         return  new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    private JwtAuthenticationFilter authFilter;
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService){
+        return new JwtAuthenticationFilter(jwtUtil, userDetailsService);
+    }
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter JwtAuthenticationFilter) throws Exception {
         http
             .csrf(csrf -> csrf.disable()) // Solo para pruebas, no recomendado en producción
             .authorizeHttpRequests(auth -> auth
@@ -35,7 +37,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/tournaments/**").hasRole("ADMIN")
                 .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class); // here put first validate token from jwt before UsernamePasswordAuthenticationFilter from security
+        http.addFilterBefore(JwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // here put first validate token from jwt before UsernamePasswordAuthenticationFilter from security
         return http.build();
     }
 }
