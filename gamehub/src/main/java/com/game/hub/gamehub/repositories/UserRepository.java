49de546e411d26b91056a3b5ac26bc1ecb2c69dc
Carlos Villanueva <cs.vp@outlook.com>
2025-06-22 20:48:01 -0600

    feat: change extends from CrudRepository to JpaRepository in UserRepository interface

--- gamehub/src/main/java/com/game/hub/gamehub/repositories/UserRepository.java
+++ gamehub/src/main/java/com/game/hub/gamehub/repositories/UserRepository.java
@@ -1,14 +1,18 @@
 package com.game.hub.gamehub.repositories;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.game.hub.gamehub.models.User;
 
-public interface UserRepository extends CrudRepository<User, UUID> {
-    List<User> findByEmail(String email);
-    User findFirstByEmail(String email); // Find the first user by email
-    User findByUsername(String username);
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByUsername(String username);
+
+	User findFirstByEmail(String email); // Find the first user by email
+
 }
