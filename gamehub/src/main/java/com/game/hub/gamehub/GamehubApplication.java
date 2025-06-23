cf4f0c398316cac30e2428faa144849645e00be9
Carlos Villanueva <cs.vp@outlook.com>
2025-06-22 20:56:19 -0600

    feat: add register user functionality

--- gamehub/src/main/java/com/game/hub/gamehub/GamehubApplication.java
+++ gamehub/src/main/java/com/game/hub/gamehub/GamehubApplication.java
@@ -1,12 +1,17 @@
 package com.game.hub.gamehub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GamehubApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GamehubApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("*************** Starting Gamehub App **************");
 		SpringApplication.run(GamehubApplication.class, args);
 	}
 
--- gamehub/src/main/java/com/game/hub/gamehub/services/AuthServiceImpl.java
+++ gamehub/src/main/java/com/game/hub/gamehub/services/AuthServiceImpl.java
@@ -1,24 +1,57 @@
 package com.game.hub.gamehub.services;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.game.hub.gamehub.dtos.LoginRequest;
 import com.game.hub.gamehub.dtos.LoginResponse;
 import com.game.hub.gamehub.dtos.RegisterRequest;
 import com.game.hub.gamehub.dtos.RegisterResponse;
+import com.game.hub.gamehub.enums.UserRank;
+import com.game.hub.gamehub.enums.UserRole;
+import com.game.hub.gamehub.models.User;
+import com.game.hub.gamehub.repositories.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final Logger logger = LoggerFactory.getLogger(AuthService.class);
 
-    @Override
-    public RegisterResponse register(RegisterRequest request) {
-        return null;
-    }
+	public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    @Override
-    public LoginResponse login(LoginRequest request) {
-        return null;
-    }
+	@Override
+	public RegisterResponse register(RegisterRequest request) {
+
+		Optional<User> userOpt = userRepository.findByEmail(request.email());
+		if (userOpt.isPresent())
+			new RuntimeException("Email already used!");
+
+		User user = new User();
+		user.setEmail(request.email());
+		user.setPassword(passwordEncoder.encode(request.password()));
+		user.setUsername(request.username());
+		user.setPoints(0);
+		user.setRank(UserRank.BRONZE);
+		user.setRole(UserRole.PLAYER);
+		userRepository.save(user);
+
+		logger.info("User registered:" + user.getUsername());
+		
+
+		return new RegisterResponse("User registered");
+	}
+
+	@Override
+	public LoginResponse login(LoginRequest request) {
+		return null;
+	}
 }
-
